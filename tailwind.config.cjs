4dc9d904be4385b4af549a49ebae3d034b220076
nakar0 <shoutaro425@gmail.com>
2023-11-06 21:22:29 +0900

    WIP: save temporaly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors: {
+      'primary': '#e9e9e5',
+      'pure-black': '#000000',
+      'light-black': "#242424"
+    },
     extend: {},
   },
   plugins: [],
