cb757b55dbfd4ec62720bf39f3500a0583ee48cc
nakar0 <shoutaro425@gmail.com>
2023-11-09 16:21:02 +0900

    Refactored

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      'primary': '#e9e9e5',
-      'pure-black': '#000000',
-      'light-black': "#242424"
+    extend: {
+      colors: {
+        primary: '#e9e9e5',
+        'pure-black': '#000000',
+        'light-black': '#242424',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
