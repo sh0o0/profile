b5baf6eae1c79e510e3b86c9d13bb74d91d304bd
nakar0 <shoutaro425@gmail.com>
2023-11-09 15:08:20 +0900

    Impled i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'EN',
+  ja: 'JA',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+  },
+  ja: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+  }
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,18 @@
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function getUrl(currentUrl: URL, path: string) {
+  const lang = getLangFromUrl(currentUrl);
+  return `/${lang}/${path}`;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
